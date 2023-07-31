5d516f8f407001b3da8e3be3eab30b8ff529653e
Nick Bicknell <nicholasbicknell@gmail.com>
2023-07-31 17:32:29 -0500

    created the writeToFile function and the init function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMD = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -54,10 +54,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log("README.md file generation successful!");
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log("Generating....");
+            writeToFile('./tests/README.md', generateMarkdown({ ...answers }));
+        })
+}
 
 // Function call to initialize app
 init();
